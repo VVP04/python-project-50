18376f3531e7cdcb1d123046d5e595c873f4e382
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-03-20 22:52:39 +0500

    test: use fixture and mark.parametrize in tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,14 +1,18 @@
+import pytest
 from gendiff import generate_diff
 from pathlib import Path
 
+@pytest.fixture
+def expected_stylish_output():
+    path = Path("tests/fixtures", "expected_stylish_output.txt").resolve()
+    with open(path, 'r') as file:
+        return file.read().strip()
 
-file1_json = Path("tests/fixtures", "file1.json").resolve()
-file2_json = Path("tests/fixtures", "file2.json").resolve()
-expected_stylish_output = Path("tests/fixtures", "expected_stylish_output.txt").resolve()
+@pytest.mark.parametrize("file1, file2", [
+    (Path("tests/fixtures", "file1.json").resolve(), Path("tests/fixtures", "file2.json").resolve()),
+    (Path("tests/fixtures", "file1.yaml").resolve(), Path("tests/fixtures", "file2.yaml").resolve()),
+])
 
-
-def test_generate_diff_with_flat_json():
-    with open(expected_stylish_output, 'r') as file:
-        expected_output = file.read().strip()
-    result = generate_diff(file1_json, file2_json)
-    assert result == expected_output
+def test_generate_diff_with_flat_files(file1, file2, expected_stylish_output):
+    result = generate_diff(file1, file2)
+    assert result == expected_stylish_output
