cd231ff54df2715aa6a35e7188f8f569c1364e5f
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-03-20 18:34:54 +0500

    test: rename test_generate_diff to test_generate_diff_with_flat_json

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -7,9 +7,8 @@ file2_json = Path("tests/fixtures", "file2.json").resolve()
 expected_stylish_output = Path("tests/fixtures", "expected_stylish_output.txt").resolve()
 
 
-def test_generate_diff():
+def test_generate_diff_with_flat_json():
     with open(expected_stylish_output, 'r') as file:
         expected_output = file.read().strip()
     result = generate_diff(file1_json, file2_json)
-    print(result)
     assert result == expected_output
