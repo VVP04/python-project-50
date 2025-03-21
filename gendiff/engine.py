b30d33df36929290876d658c911ebb67b7b6a092
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-03-21 14:59:31 +0500

    feat: add YAML files comparison

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -2,17 +2,11 @@ import json
 from pathlib import Path
 
 from gendiff.formatters.stylish import make_string
-
-
-def read_file(file):
-    file_path = Path(file).resolve()
-    with open(file_path, 'r') as json_file:
-        data = json.load(json_file)
-        return data
+from gendiff.parser import parse_file
 
 
 def generate_diff(file1, file2):
-    data1 = read_file(file1)
-    data2 = read_file(file2)
+    data1 = parse_file(file1)
+    data2 = parse_file(file2)
     diff = make_string(data1, data2)
     return diff
---
+++ gendiff/parser.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+from pathlib import Path
+
+
+def get_file_path(file):
+    file_path = Path(file).resolve()
+    return file_path
+
+
+def parse_file(file):
+    file_path = get_file_path(file)
+    format_file = file_path.suffix[1:]
+    if format_file == 'json':
+        with open(file_path, 'r') as file_obj:
+            data = json.load(file_obj)
+        return data
+    elif format_file in ('yaml', 'yml'):
+        with open(file_path, 'r') as file_obj:
+            data = yaml.safe_load(file_obj)
+        return data
+    raise ValueError(f'Unsupported file extension: {format_file}')
