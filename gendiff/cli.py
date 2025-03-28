a35c14d7b2762978015a68395f56dad36e46789c
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-03-28 19:12:03 +0500

    feat: add default format 'stylish'

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -9,8 +9,10 @@ def get_args():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument(
-        '-f', 
-        '--format',
+        '-f', '--format',
+        dest='format',
+        default='stylish',
+        type=str,
         help='set format of output',
     )
     args = parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from gendiff.cli import get_args
-from gendiff.engine import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 def main():
     args = get_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
   
     
